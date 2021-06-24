54b36887519f63495d6b6817c5aa5d99217b8530
ericgaspar <junk.eg@free.fr>
2021-06-24 09:59:49 +0200

    Cleaning up

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,7 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+
 pkg_dependencies="php-geoip php7.0-cli"
 app="emailpoubelle"
 lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"

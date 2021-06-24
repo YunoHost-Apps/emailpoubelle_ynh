9ab4afa8f968e5b3318bdbbb467f22cbed1c2f57
oufmilo <44617467+oufmilo@users.noreply.github.com>
2021-06-24 17:47:58 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-pkg_dependencies="php-geoip php7.0-cli php7.0-foobar"
+pkg_dependencies="php-geoip php7.3-cli"
 app="emailpoubelle"
 lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"

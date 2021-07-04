e21a99527ab91ec4f2537f6813f7eab38fb32db9
oufmilo <44617467+oufmilo@users.noreply.github.com>
2021-07-04 14:07:30 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,7 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-YNH_PHP_VERSION="7.3"
-extra_php_dependencies="php-geoip php${YNH_PHP_VERSION}-cli"
+YNH_PHP_VERSION="7.4"
+extra_php_dependencies="php${YNH_PHP_VERSION}-geoip php${YNH_PHP_VERSION}-cli"
 app="emailpoubelle"
 lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"

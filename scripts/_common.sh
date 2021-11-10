8b8395ab5c4fc6bbfd87fd240e363117005c644e
ericgaspar <junk.eg@free.fr>
2021-11-10 08:10:36 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,8 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+
 YNH_PHP_VERSION="7.3"
 extra_php_dependencies="php${YNH_PHP_VERSION}-geoip php${YNH_PHP_VERSION}-cli"
-app="emailpoubelle"
+
 lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"

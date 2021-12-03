342bced93965104b4542cdf64d23a9a54c299c14
ericgaspar <junk.eg@free.fr>
2021-12-03 19:28:13 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,7 @@
 #=================================================
 
 YNH_PHP_VERSION="7.3"
-extra_php_dependencies="php${YNH_PHP_VERSION}-geoip php${YNH_PHP_VERSION}-cli"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-geoip php${YNH_PHP_VERSION}-cli"
 
 lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"
